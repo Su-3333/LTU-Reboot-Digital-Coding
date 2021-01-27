425d632a00857c9b8edac35a63630b1d13d712b1
Su-3333 <75368214+Su-3333@users.noreply.github.com>
2021-01-27 06:59:19 +0000

    Update task-3.1.2.js
    Add alert, console.log and document.write

---
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -0,0 +1,9 @@
+alert('This is an alert message'); //This is a pop up message box
+
+/*
+Below is a message that will appear in the console
+*/
+
+console.log('This is a message from the console');
+
+document.write('Hello from the document');
