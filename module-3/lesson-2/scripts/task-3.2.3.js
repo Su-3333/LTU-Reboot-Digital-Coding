aab837d18b84efede8fe09b01fd472f63adf1af2
Suh <hasuh@hotmail.co.uk>
2021-01-28 21:30:04 +0000

    Update task-3.2.3.js
    Add showFullName function and return value to use later in my program

--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -10,5 +10,15 @@ var message = greet('Hello', 'Luke');
 /* EXAMPLE */
 
 // ADD YOUR CODE BELOW HERE
+function showFullName (fName, sName){
 
+  var fullName = fName + ' ' + sName +'<br>';
+  return fullName;
+
+}
+ var person1 = showFullName('Suh', 'Rashid');
+ document.write (person1);
+
+ var person2 = showFullName('Has', 'Rashid');
+ document.write (person2);
 // ADD YOUR CODE ABOVE HERE
