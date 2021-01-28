630024bad9b3ce60f0d0bfb9c021d24ff9beb0ca
Suh <hasuh@hotmail.co.uk>
2021-01-28 21:03:30 +0000

    Update task-3.2.2.js
    Add function showFullName and invoke function

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,11 @@
+function showFullName (fName, sName){
+
+    var fullName = fName + ' ' + sName;
+
+    document.write(fullName + '<br>');
+
+}
+
+showFullName("Suh", "Rashid");
+showFullName("Has", "Rashid");
+showFullName("Taha", "Rashid");
