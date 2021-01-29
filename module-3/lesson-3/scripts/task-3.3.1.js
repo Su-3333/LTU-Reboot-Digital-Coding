5d21e5bd9fca9468f7b84d78cf02f227a31be0fd
Suh <hasuh@hotmail.co.uk>
2021-01-29 20:12:59 +0000

    Update task-3.3.1.js
    create percentage calculator

---
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -0,0 +1,13 @@
+function percentageCalculator ( number, percentage) {
+    var percentageOf = (number * percentage) /100;
+    return percentageOf;
+   
+
+}
+
+
+// var number = 600
+// var percentage = 40
+console.log ( percentageCalculator ( 600, 40));
+console.log ( percentageCalculator ( 90, 20));
+console.log ( percentageCalculator( number, percentage));
