e21155b71d37d3debb18b8353e3c488c7145c5e8
Suh <hasuh@hotmail.co.uk>
2021-01-29 19:29:24 +0000

    Update task-3.2.6.js
    And operators, elseif and else statements.  Cloudy or raining?

--- module-3/lesson-2/scripts/task-3.2.6.js
+++ module-3/lesson-2/scripts/task-3.2.6.js
@@ -1,12 +1,14 @@
-var cloudy = 'yes';
-var raining = 'dry';
+var cloudy = 'no';
+var raining = 'wet';
 
 if (cloudy === 'yes' && raining === 'wet'){
     document.write ('wear a raincoat');
 } else if (cloudy === 'yes' && raining === 'no'){
     document.write ('carry a raincoat');
-} else if ( cloudy === 'no' || raining ==='dry') { 
+} else if ( cloudy === 'no' && raining ==='dry') { 
     document.write ('enjoy the dry weather');
+}else if ( cloudy === 'no' && raining ==='wet') { 
+        document.write ('you are under the water sprinkler');
 } else { document.write ('Try again');}
 
 
