900c47688086204ddc07aad52ceab3174c2ff7f1
Suh <hasuh@hotmail.co.uk>
2021-01-29 07:33:24 +0000

    Update task-3.2.5.js
    If, else if and else condition statements.

---
+++ module-3/lesson-2/scripts/task-3.2.5.js
@@ -0,0 +1,17 @@
+var temp = 25;
+
+if (temp < 0) {
+    document.write("Stay inside");
+}
+    else if (temp< 30) {
+    document.write('please put your coat and hat on');
+}
+    else if (temp< 50){
+    document.write("Please put on your coat");
+
+}
+    else {
+        document.write('No need for your hats or coats, its lovely')
+    }
+
+
