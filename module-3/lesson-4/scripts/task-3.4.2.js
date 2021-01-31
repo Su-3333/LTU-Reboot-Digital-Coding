21ffb551c9b22ea26fa1525b21645f406af294f9
Suh <hasuh@hotmail.co.uk>
2021-01-31 20:10:03 +0000

    Create task-3.4.2.js
    Add an array and loop over it using a for loop

---
+++ module-3/lesson-4/scripts/task-3.4.2.js
@@ -0,0 +1,8 @@
+var favouriteFoods = [ 'doughnuts' , 'burgers', 'icecream', 'cheese', 'biscuits'];
+
+for( var index=0; index < favouriteFoods.length; index++  ){
+
+    console.log (favouriteFoods[index]);
+
+
+}
