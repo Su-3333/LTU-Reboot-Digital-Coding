12c96df7ccc6f8f1bd7f837d4bd2160e65fd776e
Suh <hasuh@hotmail.co.uk>
2021-01-31 20:59:38 +0000

    Create task-3.4.3.js
    Index and for loop for favourite foods. - incomplete

---
+++ module-3/lesson-4/scripts/task-3.4.3.js
@@ -0,0 +1,7 @@
+var favouriteFoods = [ 'doughnuts' , 'burgers', 'icecream', 'cheese', 'biscuits'];
+
+for( var index=0; index < favouriteFoods.length; index++  )
+
+   { document.write ("Your favourite foods is " + favouriteFoods[index ]);}
+   
+    console.log (favouriteFoods[index]);
