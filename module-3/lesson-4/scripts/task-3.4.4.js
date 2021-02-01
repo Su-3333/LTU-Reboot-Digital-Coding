64a4ef2d5e88006caaf3124a4163d5cdc80f6160
Suh <hasuh@hotmail.co.uk>
2021-02-01 08:04:35 +0000

    Create task-3.4.4.js
    create favourtieRecipe object and loop over ingredients and directions

---
+++ module-3/lesson-4/scripts/task-3.4.4.js
@@ -0,0 +1,24 @@
+var  favouriteRecipe = {
+    recipeTitle : "veganChiaPudding",
+    servings: 4,
+    ingredients: [
+        "3 tbs chia seeds", 
+        "500ml almond milk",  
+        "1 tsp vanilla powder", 
+        "1/2 tsp cinnamon powder"
+    ],
+    directions: [
+        "Measure out ingredients", 
+        "add all to a large bowl", 
+        "Whisk, then refigerate"
+    ],
+}
+    var ingredients = favouriteRecipe.ingredients;
+        for (var index=0; index < ingredients.length; index++){
+            console.log(ingredients[index]);
+        }
+    var directions = favouriteRecipe.directions;
+        for (var index=0; index < directions.length; index++) {
+            console.log (directions[index]);
+        }
+        
