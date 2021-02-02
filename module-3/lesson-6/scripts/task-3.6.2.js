8fb17094b25ee7f154978da2d166a862e1fb3364
Suh <hasuh@hotmail.co.uk>
2021-02-02 20:44:38 +0000

    Update task-3.6.2.js
    added in comments to understand code.

--- module-3/lesson-6/scripts/task-3.6.2.js
+++ module-3/lesson-6/scripts/task-3.6.2.js
@@ -1,6 +1,14 @@
+
+// what are we attaching it to.
 let div = document.querySelectorAll('div')[0];
+
+// new paragraph in limbo
 let newParagraph = document.createElement ('p');
+// new text
 let newTextNode = document.createTextNode('This is the new text Node');
 
+// attach new textnode to new paragraph
 newParagraph.appendChild( newTextNode );
+
+// added new paragraph to div.
 div.appendChild (newParagraph);
