c4ae170af659996be9faf72eb25d59d8a396420e
Suh <hasuh@hotmail.co.uk>
2021-02-02 10:53:50 +0000

    Create task-3.6.1.js
    Select img and changed attribute src attribute, select 2nd paragraph and add styles

---
+++ module-3/lesson-6/scripts/task-3.6.1.js
@@ -0,0 +1,8 @@
+let logo = document.querySelector('img');
+    logo.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/07._Camel_Profile%2C_near_Silverton%2C_NSW%2C_07.07.2007.jpg/440px-07._Camel_Profile%2C_near_Silverton%2C_NSW%2C_07.07.2007.jpg');
+
+let secondParagraph = document.querySelectorAll('p')[1];
+    secondParagraph.style.border = '4px solid goldenrod';
+    secondParagraph.style.marginTop = '50px';
+    secondParagraph.style.padding = '50px';
+    
