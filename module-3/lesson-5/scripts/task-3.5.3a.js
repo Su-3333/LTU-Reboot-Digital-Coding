bfc47aa6e4366d4d29dc7351f29b925cf8cdeaab
Suh <hasuh@hotmail.co.uk>
2021-02-02 20:10:44 +0000

    Update task-3.5.3a.js
    introduced print and var, and template literal.
    
    Added in Lukes code for styling.  need to further understand this.......

--- module-3/lesson-5/scripts/task-3.5.3a.js
+++ module-3/lesson-5/scripts/task-3.5.3a.js
@@ -5,12 +5,12 @@ function discountProductType (arr, discountAmount, type){
     for(index = 0; index < arr.length; index++) {
         if (arr[index].type === type) {
             
-            let discount = (arr[index].price * "discountAmount") /100 ;
+            let discount = (arr[index].price * discountAmount) /100 ;
             totalPrice = totalPrice + ( arr[index].price - discount) * arr[index].quantity;
         }
 
             else if (type === "any") {
-                let discount = (arr[index].price * "discountAmount") /100 ;
+                let discount = (arr[index].price * discountAmount) /100 ;
                 totalPrice = totalPrice + ( arr[index].price - discount) * arr[index].quantity;
             }
             else{
@@ -23,8 +23,22 @@ function discountProductType (arr, discountAmount, type){
 
       
 }
+// version 1
+console.log("£" + discountProductType( shoppingCart, 20, "home"));
+// above is a return, will not print.  need console.log 
+// concatination
 
-discountProductType( shoppingCart, 20, "home"); 
 
- console.log ("£" +discountProductType(shoppingCart));
+// version 2
+var discountHome = discountProductType( shoppingCart, 20, "home");
+    console.log(`£ ${discountHome}`  );
+// template literal
+
+//my version.
+ console.log ("£" + discountProductType(shoppingCart));
     
+
+ document.write( '<div class="alert alert-success" role="alert">All products have 20% Discount : £ ' + discountProductType( shoppingCart, 20, 'any' ) + '</div>');
+ document.write( '<div class="alert alert-warning" role="alert">All products with type of food have 25% Discount : £ ' + discountProductType( shoppingCart, 25, 'food' ) + '</div>');
+ document.write( '<div class="alert alert-primary" role="alert">All products with type of alcohol have 20% Discount : £ ' + discountProductType( shoppingCart, 20, 'alcohol' ) + '</div>');
+ document.write( '<div class="alert alert-danger" role="alert">All products with type of home have 20% Discount : £ ' + discountProductType( shoppingCart, 20, 'home' ) + '</div>');
